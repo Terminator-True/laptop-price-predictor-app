390f1cc8b53ca616eda2f2be2641fd1f8bd157d7
Terminator-True <jf28renault@gmail.com>
2024-05-02 20:38:35 +0200

    ADD: Initial Endpoint code

---
+++ backend/Models/Prediction.py
@@ -0,0 +1,43 @@
+class Prediction:
+    
+    marca:int
+    inches:float
+    cpu:int
+    ram:int
+    graphic_card:int
+    so:str
+    ssd:int
+    
+    def __init__(self) -> None:
+        self.cpus = [
+            'low gamma intel processor',
+            'low gamma amd processor',
+            'intel core i5',
+            'intel core i7',
+            'intel core i9',
+            'amd ryzen 5',
+            'amd ryzen 7',
+            'amd ryzen 9'
+            'Mac Processor',
+        ]
+        self.graphic_cards = [
+            'Nvidia High gamma',
+            'Nivida medium gamma',
+            'Nvidia low gamma',
+            'Other nvidia grafic card',
+            'integrated graphics',
+            'Other nvidia grafic card',
+            'Apple integrated graphics',
+            'Amd low Gamma',
+            'Amd High gamma'
+        ]
+        
+    def get_cpu(self,cpu_num):
+        return self.cpus[cpu_num]
+    
+    def get_cpu(self,graphic_card_num):
+        return self.graphic_cards[graphic_card_num]
+    
+    def do_prediction(self):
+        #TODO Call to prediction model
+        pass
--- backend/main.py
+++ backend/main.py
@@ -16,11 +16,7 @@ class Item(BaseModel):
 def read_root():
     return 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
 
-
-@app.put("/items/{item_id}")
+@app.post("/predictor")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
