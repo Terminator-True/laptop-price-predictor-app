78cf33dbe8f3f6434af15169e7308a51c942d52f
Terminator-True <jf28renault@gmail.com>
2024-05-07 13:12:23 +0200

    ADD: Prediction class + test

--- backend/models/Prediction.py
+++ backend/models/Prediction.py
@@ -1,14 +1,24 @@
+import pickle
+import numpy as np
+
 class Prediction:
     
     marca:int
     inches:float
     cpu:int
     ram:int
-    graphic_card:int
-    so:str
+    gpu:int
+    so:int
     ssd:int
     
-    def __init__(self) -> None:
+    def __init__(self, marca, inches, cpu, ram, gpu, so, ssd) -> None:
+        self.marca = marca
+        self.inches = inches
+        self.cpu = cpu
+        self.ram = ram
+        self.gpu = gpu
+        self.so = so
+        self.ssd = ssd
         self.cpus = [
             'low gamma intel processor',
             'low gamma amd processor',
@@ -20,7 +30,7 @@ class Prediction:
             'amd ryzen 9'
             'Mac Processor',
         ]
-        self.graphic_cards = [
+        self.gpus = [
             'Nvidia High gamma',
             'Nivida medium gamma',
             'Nvidia low gamma',
@@ -61,12 +71,37 @@ class Prediction:
             "Jetwing",
             "Toshiba"
         ]
-    def get_cpu(self,cpu_num):
-        return self.cpus[cpu_num]
+        self.sos = [
+            'No Os/ Linux',
+            'Windows'
+        ]
+    def get_cpu(self):
+        return self.cpus[self.cpu]
     
-    def get_cpu(self,graphic_card_num):
-        return self.graphic_cards[graphic_card_num]
+    def get_gpu(self):
+        return self.gpus[self.gpu]
+    
+    def get_so(self):
+        if self.marca == 'Apple':
+            return 'MacOs'
+        return self.sos[self.so]
+    def get_marca(self):
+        return self.marcas[self.marca]
     
     def do_prediction(self):
         #TODO Call to prediction model
-        pass
+        pipe = pickle.load(open('backend\ML_model\pipe_main.pkl','rb'))
+        query = np.array([
+            self.get_marca(),
+            self.inches,
+            self.get_cpu(),
+            self.ram,
+            self.get_gpu(),
+            self.get_so(),
+            self.ssd], dtype=object)
+        query = query.reshape(1,7)
+        return int(np.exp(pipe.predict(query)[0]))
+    
+if __name__ == '__main__':
+    prediction = Prediction(0,15.6,0,32,0,0,2000)
+    print(prediction.do_prediction())
