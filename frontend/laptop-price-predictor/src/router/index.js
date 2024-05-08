779e188a669baf485b63fcec6a6c6db7c195d2ac
Terminator-True <jf28renault@gmail.com>
2024-05-08 15:27:56 +0200

    ADD: HomeViewApp mínimal

--- frontend/laptop-price-predictor/src/router/index.js
+++ frontend/laptop-price-predictor/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeViewApp from '../views/HomeView.vue'
+import HomeViewApp from '../views/HomeViewApp.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,14 +9,14 @@ const router = createRouter({
       name: 'home',
       component: HomeViewApp
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/AboutView.vue')
+    // }
   ]
 })
 
--- frontend/laptop-price-predictor/tailwind.config.js
+++ frontend/laptop-price-predictor/tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [    
+    './src/**/*.vue',
+],
   theme: {
     extend: {},
   },
